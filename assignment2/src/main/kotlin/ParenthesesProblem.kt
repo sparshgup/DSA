fc58a4ccfca5e1d025008b02324d6576ff7abc38
Sparsh Gupta <sparshg@gmail.com>
2024-01-31 15:33:31 -0500

    A2: added comments to P4

--- assignment2/src/main/kotlin/ParenthesesProblem.kt
+++ assignment2/src/main/kotlin/ParenthesesProblem.kt
@@ -1,14 +1,18 @@
 class ParenthesesProblem {
     fun isValid(s: String): Boolean {
         val stack = Stack<Char>()
+        // loop through the characters in the string
         for (char in s) {
             when (char) {
+                // push to stack if it is an opening parenthesis
                 '(', '{', '[' -> stack.push(char)
+                // determine if a closing parenthesis is valid
                 ')' -> if (stack.isEmpty() || stack.pop() != '(') return false
                 '}' -> if (stack.isEmpty() || stack.pop() != '{') return false
                 ']' -> if (stack.isEmpty() || stack.pop() != '[') return false
             }
         }
+        // return true if the stack is exhausted and the parentheses string is valid
         return stack.isEmpty()
     }
 }
